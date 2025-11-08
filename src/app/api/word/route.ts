11f39e35a1750f75bb3fc5b4a77feaeb005f35bd
supichaprom-cell <supicha.prom@bumail.net>
2025-11-08 14:34:47 +0700

    daily_vocab_web

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { words } from "@/data/words";
 
 export async function GET() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const word = words[randomIndex];
-  return NextResponse.json({ word });
+  const response = await fetch('http://localhost:8000/api/word');
+  const data = await response.json();
+  return NextResponse.json({ data });
 }
